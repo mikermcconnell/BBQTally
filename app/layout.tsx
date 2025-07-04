c598dcfcc5467e27dd61f18fbb67954f9839c590
mikermcconnell <80167613+mikermcconnell@users.noreply.github.com>
2025-07-03 21:46:33 -0400

    Add Google AdSense verification code for monetization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import React from 'react'
 import './globals.css'
 
@@ -44,7 +45,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4725000956854577"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+        {children}
+      </body>
     </html>
   )
 } 
